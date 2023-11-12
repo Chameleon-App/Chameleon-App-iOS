689652946d29def7f381d443df9d610a76005139
ILIA CHUB <ilia.chub@dev.mobileup.ru>
2023-11-12 17:21:17 +0300

    [CHAIOS-8]: XcodeGen setup

---
+++ Sources/ChameleonApp.swift
@@ -0,0 +1,19 @@
+//
+//  ChameleonApp.swift
+//  chameleon
+//
+//  Created by Ilia Chub on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChameleonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                Text("Chameleon App")
+            }
+        }
+    }
+}
