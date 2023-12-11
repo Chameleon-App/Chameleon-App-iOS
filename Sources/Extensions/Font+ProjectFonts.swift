1dcb99d765e28a5dfccae66212c7289780c0f28c
ILIA CHUB <foggydawning@gmail.com>
2023-12-11 18:26:27 +0300

    [CHAIOS-17]: ColorFeedView (#5)
    * [CHAIOS-17]: ColorFeedView
    
    * [CHAIOS-17]: ColorFeedView. Part 2

---
+++ Sources/Extensions/Font+ProjectFonts.swift
@@ -0,0 +1,12 @@
+//
+//  Font+ProjectFonts.swift
+//  chameleon
+//
+//  Created by Ilia Chub on 11.12.2023.
+//
+
+import SwiftUI
+
+extension Font {
+    static var bodySmall: Font { .system(size: 13, weight: .regular) }
+}
