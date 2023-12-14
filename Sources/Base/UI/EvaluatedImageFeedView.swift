d31399226182ef010659d08f75b94f095ef7551b
ILIA CHUB <foggydawning@gmail.com>
2023-12-15 00:49:54 +0300

    [CHAIOS-16]: EvaluatedImageFeedView (#12)
    * [CHAIOS-16]: EvaluatedImageFeedView
    
    (cherry picked from commit 24f59e01f502bb66135db4499b64702e585cec9f)
    
    * [CHAIOS-16]: EvaluatedImageFeedView. Part 2

---
+++ Sources/Base/UI/EvaluatedImageFeedView.swift
@@ -0,0 +1,59 @@
+//
+//  EvaluatedImageFeedView.swift
+//  chameleon
+//
+//  Created by Ilia Chub on 15.12.2023.
+//
+
+import SwiftUI
+
+struct EvaluationFeedImageViewItem {
+    let url: URL?
+    let points: Int
+    let date: Date?
+    let authorImageUrl: URL?
+}
+
+struct EvaluatedImageFeedView: View {
+    let viewItem: EvaluationFeedImageViewItem
+    
+    var body: some View {
+        ZStack {
+            AsyncImageView(url: viewItem.url)
+            HStack {
+                Spacer()
+                VStack {
+                    Spacer()
+                    Text(String(viewItem.points))
+                        .font(.bodySmall)
+                        .foregroundStyle(Color(.textSecondary))
+                        .padding(.horizontal, 4)
+                        .padding(.vertical, 1)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(5)
+                        .padding(.all, 5)
+                }
+            }
+            HStack {
+                VStack {
+                    if let authorImageUrl = viewItem.authorImageUrl {
+                        AsyncImageView(url: authorImageUrl, rounding: .max)
+                            .frame(width: 32, height: 32)
+                            .padding(.all, 3)
+                    } else if let date = viewItem.date {
+                        Text(DateService.getFormattedDate(date: date, format: .monthAndDate) ?? .empty)
+                            .font(.bodySmall)
+                            .foregroundStyle(Color(.textSecondary))
+                            .padding(.horizontal, 4)
+                            .padding(.vertical, 1)
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(5)
+                            .padding(.all, 5)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+}
---
+++ Sources/Services/DateService.swift
@@ -0,0 +1,45 @@
+//
+//  DateService.swift
+//  chameleon
+//
+//  Created by Ilia Chub on 15.12.2023.
+//
+
+import Foundation
+
+final class DateService {
+    enum DateFormats {
+        case monthAndDate
+        case custom(format: String)
+    }
+    
+    static func getFormattedDate(date: Date, format: DateFormats) -> String? {
+        switch format {
+        case .monthAndDate:
+            return getCustomDate(date: date, format: "MMM. dd")
+        case .custom(let format):
+            return getCustomDate(date: date, format: format)
+        }
+    }
+    
+    static func getFormattedDate(timeInterval: TimeInterval, format: DateFormats) -> String? {
+        let date = Date(timeIntervalSince1970: timeInterval)
+        
+        return getFormattedDate(date: date, format: format)
+    }
+    
+    static private func getCustomDate(date: Date, format: String) -> String? {
+        let formatter = getDateFormatter()
+        formatter.dateFormat = format
+        
+        return formatter.string(from: date)
+    }
+    
+    static private func getDateFormatter() -> DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en")
+        
+        return dateFormatter
+    }
+}
+
