5c68890018518551a2ee82f9ad1a215a4d88db6b
ILIA CHUB <foggydawning@gmail.com>
2023-12-15 00:22:52 +0300

    [CHAIOS-13]: AsyncImageView (#11)
    * [CHAIOS-13]: AsyncImageView
    
    * [CHAIOS-13]: AsyncImageView. Part 2

---
+++ Sources/Base/UI/AsyncImageView.swift
@@ -0,0 +1,39 @@
+//
+//  AsyncImageView.swift
+//  chameleon
+//
+//  Created by Ilia Chub on 15.12.2023.
+//
+
+import SwiftUI
+
+enum AsyncImageRounding {
+    case radius(Double)
+    case max
+}
+
+struct AsyncImageView: View {
+    let url: URL?
+    let rounding: AsyncImageRounding?
+    
+    var body: some View {
+        AsyncImage(url: url) {
+            $0
+                .resizable()
+        } placeholder: {
+            SkeletonView()
+        }
+        .cornerRadius(getCornerRadius())
+    }
+    
+    private func getCornerRadius() -> CGFloat {
+        switch rounding {
+        case .radius(let radius):
+            return radius
+        case .max:
+            return CGFloat(Int.max)
+        case .none:
+            return .zero
+        }
+    }
+}
