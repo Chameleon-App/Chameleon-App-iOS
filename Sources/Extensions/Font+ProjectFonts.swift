73c8fe0950bf0e83dc5cac3251074cefaf575df8
ILIA CHUB <ilia.chub@dev.mobileup.ru>
2023-12-18 20:50:10 +0300

    [CHAIOS-31]: LogIn and authentication screens. Part 5

--- Sources/Extensions/Font+ProjectFonts.swift
+++ Sources/Extensions/Font+ProjectFonts.swift
@@ -10,4 +10,5 @@ import SwiftUI
 extension Font {
     static var bodySmall: Font { .system(size: 13, weight: .regular) }
     static var bodyBig: Font { .system(size: 16, weight: .bold) }
+    static var headingPrimary: Font { .system(size: 24, weight: .heavy) }
 }
--- Sources/Modules/Login/LoginView.swift
+++ Sources/Modules/Login/LoginView.swift
@@ -8,10 +8,35 @@
 import SwiftUI
 
 struct LoginView: View {
+    private enum Constants {
+        static let loginTitleKey = "loginTitleKey"
+    }
+    
     @ObservedObject var viewModel: LoginViewModel
     
     var body: some View {
-        Color.red
-            .ignoresSafeArea()
+        ZStack {
+            VStack(alignment: .leading) {
+                Color(.backgroundAccent)
+                    .ignoresSafeArea()
+                    .frame(height: 180)
+                Spacer()
+                    .frame(height: 8)
+                Text(String(localized: String.LocalizationValue(Constants.loginTitleKey)))
+                    .font(.headingPrimary)
+                    .padding(.horizontal, 14)
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                    .frame(height: 110)
+                HStack {
+                    Image(.chameleon)
+                    Spacer()
+                }
+                .padding(.horizontal, 14)
+                Spacer()
+            }
+        }
     }
 }
