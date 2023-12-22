4d32f5ef4e1e15848001897fb31da4322b164113
Pavlentiy <pavel@MacBook-Pro-Pavlentiy.local>
2023-12-22 23:21:07 +0300

    remove TimerService

--- Sources/Services/StoriesService.swift
+++ Sources/Services/StoriesService.swift
@@ -12,25 +12,30 @@ enum ProgressMovingDirection {
     case forward
 }
 
-class StoriesService: TimerService {
+final class StoriesService {
     private enum Constants {
         static let storyLengthInSeconds = 5
         static let storyTimerUpdateRateInSeconds = 0.1
         static let progressUpdateValue = storyTimerUpdateRateInSeconds / Double(storyLengthInSeconds)
     }
+    
+    var isTimerPassed: Bool { currentDate >= endDate }
+    var isItFirstTimerStart: Bool { timer == nil }
         
+    private let calendar = Calendar.current
+    
+    private let timerInterval = TimeInterval(Constants.storyLengthInSeconds)
+    private let timerUpdateRate = TimeInterval(Constants.storyTimerUpdateRateInSeconds)
+    
+    private var timer: Timer?
+    private var endDate = Date()
+    private var currentDate = Date()
+    
     private var progress = Double.zero
     
     var onProgressChangeAction: Closure.Double?
     
-    init() {
-        super.init(
-            timerInterval: TimeInterval(Constants.storyLengthInSeconds),
-            timerUpdateRate: TimeInterval(Constants.storyTimerUpdateRateInSeconds)
-        )
-    }
-    
-    override func handleRemainingTimeChange(newRemainingTimeComponents: DateComponents?) {
+    func handleRemainingTimeChange(newRemainingTimeComponents: DateComponents?) {
         progress += Constants.progressUpdateValue
         
         if newRemainingTimeComponents == nil {
@@ -51,5 +56,37 @@ class StoriesService: TimerService {
         }
                 
         startTimer()
+    }
+    
+    func startTimer(currentDate: Date = .now, endDate: Date? = nil) {
+        self.currentDate = currentDate
+        self.endDate = endDate ?? (currentDate + timerInterval)
+        
+        handleRemainingTimeChange(newRemainingTimeComponents: getRemainingTime())
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: timerUpdateRate, repeats: true) { [weak self] _ in
+            guard let self else {
+                return
+            }
+            
+            self.currentDate.addTimeInterval(timerUpdateRate)
+            
+            handleRemainingTimeChange(newRemainingTimeComponents: getRemainingTime())
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func getRemainingTime() -> DateComponents? {
+        guard currentDate < endDate else {
+            stopTimer()
+            
+            return nil
+        }
+        
+        return calendar.dateComponents([.minute, .second], from: currentDate, to: endDate)
     }
 }
